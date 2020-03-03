28065357b257a0de1c6abf647afc919bdcb29e37
Ibrahim <Alaoopeyemi101@gmail.com>
2020-03-03 21:19:09 +0100

    add character set qualifier and grouping

--- regex.js
+++ regex.js
@@ -31,8 +31,6 @@ try {
 }
 */
 
-
-
 ///////////////REGULAR EXPRESSION///////
 /*
 let re;
@@ -71,26 +69,59 @@ const str = 'Hello There';
 const newStr = str.replace(re, 'Hi') //returns "HI there"; it finds the match of re and replace it with whatever is  passed in the replace
 */
 
-
 ///////////MetaCharacter Symbols
 
 let re;
 //Literal Characters
-re = /hello/;  //null because of case flag absense
-re = /hello/i;  //matches
+re = /hello/; //null because of case flag absense
+re = /hello/i; //case insensitive flag
 
 // MetaCharacters symbols
-re = /^h/i;  //means must start with: does the str starts with the letter passed
-re = /d$/i;  // must end with: true if the string ends with letter / word passed in
-re = /^hello$/i;  //means must start and end with the word/letter passed
-re = /h.llo/i;  //it defines one(any) character in the dot's spot but not more than 1 character 
-re = /h*llo/i;  //matches any character 0 or more times in the spot the astericks is
-re = /gra?e?y/i;  // optional character: means the both characters are optional,you can either putor remove any of both
-re = /gra?e?y\?/i;   // escape character: it escapes the question mark,now if ? is not in the match it throws null
-re = /gra?e?y\??/i;  //optionated the ?
-
+re = /^h/i; //means must start with the letter passed
+re = /d$/i; // must end with: true if the string ends with the letter / word passed in
+re = /^hello$/i; //means must start and end with the word/letter passed
+re = /h.llo/i; //it defines one(any) character in the dot's spot but not more than 1 character
+re = /h*llo/i; //matches any character 0 or more times in the spot the astericks is
+re = /gra?e?y/i; // optional character: means the both characters are optional,you can either putor remove any of both
+re = /gra?e?y\?/i; // escape character: it escapes the question mark,now if ? is not in the match it throws null
+re = /gra?e?y\??/i; //optionated the ?
 
 //BRACKET [] - CHARACTER SETS
+
+re = /gr[ae]y/i; //must be a or e in that postion
+re = /[GF]ray/; //must be G or F in that postion
+re = /[^GFgf]ray/; //anything but G and F in that position, case insensitive without i
+re = /^[GF]ray/i; // this is the must start with G/F format
+re = /[A-Z]ray/; //matches any uppercase letter
+re = /[a-z]ray/; //matches any lowercase letter
+re = /[A-Za-z]ray/; //matches any case letter, any letter
+re = /[0-9]ray/; //matches any digit: nb- we can pass as many numbers in its position, it would only see the last digit
+re = /[0-9][0-9]ray/; //more than one digit
+
+// BRACES {} - QUALIFIERS
+re = /hel{2}o/i;  // l has to occur at the exact number passsed
+re = /hel{2,5}o/i;  // l has to occur at the exact range of number passsed
+re = /hel{2,}o/i;  // l has to occur at least the number passed times
+
+
+// Parentheses () - grouping
+
+re = /[0-9]x{3}/;  //produces any digit and xxx, but if we want any digit and x in 3 places;
+re = /([0-9]x){3}/;  // but if we put more than any digit and x in 3 places, this would still work, if we want just exact digit and x in 3 places we write;
+re = /^([0-9]x){3}$/;  
+
+
+//Shorthand Character Classes
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -98,38 +129,18 @@ re = /gra?e?y\??/i;  //optionated the ?
 
 
 // string to match
-const str = 'gray';
-
+const str = "2x2x2x2x";
 
 // log Result
 const result = re.exec(str);
 console.log(result);
 
 function reTest(re, str) {
-    if(re.test(str)) {
-        console.log(`${str} matches ${re.source}`)
-    }else {
-        console.log(`${str} does NOT match ${re.source}`);
-    }
+ if (re.test(str)) {
+  console.log(`${str} matches ${re.source}`);
+ } else {
+  console.log(`${str} does NOT match ${re.source}`);
+ }
 }
 
 reTest(re, str);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
